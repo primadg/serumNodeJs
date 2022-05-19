bcfc1c9ba96594228fe7a90327081c7181d1c9e4
Mikhail Zub <mikhail.a.zub@gmail.com>
2022-05-19 14:11:52 +0300

    rewrite Rsocket -> websockets

--- helpers/websocketHelper.js
+++ helpers/websocketHelper.js
@@ -1,37 +1,85 @@
 //Helper for rsocket modules
+const http = require("http");
+const WebSocket = require("ws");
 const config = require("../config/appConfig");
 const StreamHandler = require("../routes/internalAPI/requestStream");
+const { app } = require("../routes/webServer");
 
-const rSocketPort = config.rSocketPort;
-const rSocketHost = config.rSocketHost;
-const transportOpts = {
-  host: rSocketHost,
-  port: rSocketPort,
-};
+const socketPort = config.rSocketPort;
 
-const getRequestHandler = () => {
-  function handleRequestStream(payload) {
-    return StreamHandler.handleRequestStream(payload);
+function noop() {}
+
+function* generateStreemId() {
+  let id = 1;
+  while (true) {
+    yield id++;
   }
-  return {
-    requestStream: handleRequestStream,
-  };
-};
+}
+
+function* generateSocketId() {
+  let id = 1;
+  while (true) {
+    yield id++;
+  }
+}
+
+const getStreemID = generateStreemId();
+
+const getSocketID = generateSocketId();
+
+const intervals = [];
 
 function websocketServer() {
-  const { RSocketServer } = require("rsocket-core");
-  const RSocketWebSocketServer = require("rsocket-websocket-server");
-  const WebSocketTransport = RSocketWebSocketServer.default;
-  const transport = new WebSocketTransport(transportOpts);
+  const server = http.createServer(app);
 
-  const rSocketServer = new RSocketServer({
-    transport,
-    getRequestHandler,
+  const webSocketServer = new WebSocket.Server({ server });
+
+  webSocketServer.on("connection", (ws) => {
+    const newSocketId = getSocketID.next().value;
+    intervals[newSocketId] = [];
+    ws.on("message", (message) => {
+      const parsedMessage = JSON.parse(message);
+      if (parsedMessage.id) {
+        clearInterval(intervals[newSocketId][parsedMessage.id]);
+      }
+      if (parsedMessage.payload) {
+        const newId = getStreemID.next().value;
+        intervals[newSocketId][newId] = StreamHandler.handleRequestStream(parsedMessage, ws, newId);
+      }
+    });
+
+    ws.onclose = () => {
+      intervals[newSocketId].forEach((el) => {
+        clearInterval(el);
+      });
+    };
+
+    ws.on("error", (e) => ws.send(e));
+
+    ws.on("pong", heartbeat);
+
+    ws.send(JSON.stringify({ type: "id", ID: newSocketId }));
   });
 
-  console.log(`Server starting on port ${rSocketPort}...`);
+  function heartbeat() {
+    this.isAlive = true;
+  }
 
-  rSocketServer.start();
+  const interval = setInterval(function ping() {
+    webSocketServer.clients.forEach((ws) => {
+      if (ws.isAlive === false) {
+        intervals[ws].forEach((interval) => {
+          clearInterval(interval);
+        });
+        return ws.terminate();
+      }
+
+      ws.isAlive = false;
+      ws.ping(noop);
+    });
+  }, 60000);
+
+  server.listen(socketPort, () => console.log(`Websocket server started on port ${socketPort}`));
 }
 
 module.exports = {
--- routes/internalAPI/requestStream.js
+++ routes/internalAPI/requestStream.js
@@ -1,38 +1,35 @@
-const { Flowable } = require("rsocket-flowable");
 const store = require("../../redux/store");
 
-function handleRequestStream(payload) {
-  const name = JSON.parse(payload.data);
+function handleRequestStream(message, ws, id) {
+  const { event, payload } = message;
+  if (event !== "orderbook") {
+    return;
+  }
+  const { name } = payload;
   if (!name) return;
   let currentOrderbookData;
   let lastOrderbookData;
-  let interval;
-  return new Flowable((subscriber) => {
-    subscriber.onSubscribe({
-      cancel: () => {
-        clearInterval(interval);
-      },
-      request: (n) => {
-        interval = setInterval(() => {
-          const { bidsL2 = [], asksL2 = [] } = store.getState().orderbookDataReducer[JSON.parse(payload.data)] || {};
-          currentOrderbookData = {
-            bids: bidsL2,
-            asks: asksL2,
-          };
-          if (JSON.stringify(currentOrderbookData) !== JSON.stringify(lastOrderbookData)) {
-            subscriber.onNext({
-              data: JSON.stringify(currentOrderbookData),
-              metadata: null,
-            });
-            lastOrderbookData = {
-              bids: bidsL2,
-              asks: asksL2,
-            };
-          }
-        }, 2000);
-      },
-    });
-  });
+  let interval = setInterval(() => {
+    const { bidsL2 = [], asksL2 = [] } = store.getState().orderbookDataReducer[name] || {};
+    currentOrderbookData = {
+      bids: bidsL2,
+      asks: asksL2,
+    };
+    if (JSON.stringify(currentOrderbookData) !== JSON.stringify(lastOrderbookData)) {
+      ws.send(
+        JSON.stringify({
+          id,
+          data: currentOrderbookData,
+          metadata: null,
+        })
+      );
+      lastOrderbookData = {
+        bids: bidsL2,
+        asks: asksL2,
+      };
+    }
+  }, 2000);
+  return interval;
 }
 
 module.exports = {
